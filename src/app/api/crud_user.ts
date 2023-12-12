e9940b5709c2cad293921f9dd1c74ea892090cad
RickC1218 <ricardoerazoliga@gmail.com>
2023-12-12 15:57:49 -0500

    fix crud_user file

--- src/app/api/crud_user.ts
+++ src/app/api/crud_user.ts
@@ -4,18 +4,26 @@ const API_BASE_URL = 'http://127.0.0.1:8000';
 
 const crud_user = {
 
-    // Obtener valor de localStorage
+    // Obtain session token
     getLocalStorageValue: (key: string) => {
         return localStorage.getItem(key);
     },
 
-    // Operación CREATE (POST)
+    // Operation sign up
     createUser: async (userData: any) => {
         try {
             const response = await axios.post(`${API_BASE_URL}/user/sign-up/`, userData);
             return response.data;
         } catch (error) {
-            throw error
+            const responseError = error as { response?: { status?: number; data?: { mensaje?: string } } };
+            switch (responseError?.response?.status) {
+                case 400:
+                    return "Error al guardar el usuario";
+                case 402:
+                    return "Este usuario ya existe";
+                default:
+                    return "Error desconocido";
+            }
         }
     },
 
@@ -25,11 +33,31 @@ const crud_user = {
             const response = await axios.post(`${API_BASE_URL}/user/set-email-verification/`, { email: email, });
             return response.data;
         } catch (error) {
-            throw error;
+            const responseError = error as { response?: { status?: number; data?: { mensaje?: string } } };
+            if (responseError?.response?.status === 404) {
+                return "Usuario no encontrado";
+            } else {
+                return "Error desconocido";
+            }
         }
     },
 
-    // Operación READ (GET)
+    // Restore password
+    restorePassword: async (email: string) => {
+        try {
+            const response = await axios.post(`${API_BASE_URL}/user/restore-password/`, { email: email, });
+            return response.data;
+        } catch (error) {
+            const responseError = error as { response?: { status?: number; data?: { mensaje?: string } } };
+            if (responseError?.response?.status === 404) {
+                return "Usuario no encontrado";
+            } else {
+                return "Error desconocido";
+            }
+        }
+    },
+
+    // Operation log in
     login: async (userData: any) => {
         try {
             const response = await axios.post(`${API_BASE_URL}/user/sign-in/`, userData);
@@ -37,17 +65,19 @@ const crud_user = {
         } catch (error) {
             const responseError = error as { response?: { status?: number; data?: { mensaje?: string } } };
             switch (responseError?.response?.status) {
+                case 400:
+                    return "Correo electrónico y contraseña no ingresados";
                 case 401:
                     return "Contraseña incorrecta";
                 case 404:
                     return "Usuario no encontrado";
-                case 400:
-                    return "Correo electrónico y contraseña no ingresados";
                 default:
                     return "Error desconocido";
             }
         }
     },
+    
+    // Operation log out
     logout: async (userData: any, session_token: string) => {
         try {
             const headers = {
@@ -56,11 +86,21 @@ const crud_user = {
             const response = await axios.put(`${API_BASE_URL}/user/sign-out/`, userData, { headers });
             return response.data;
         } catch (error) {
-            throw error;
+            const responseError = error as { response?: { status?: number; data?: { mensaje?: string } } };
+            switch (responseError?.response?.status) {
+                case 400:
+                    return "Error al cerrar sesión";
+                case 401:
+                    return "Acceso no autorizado";
+                case 404:
+                    return "Usuario no encontrado";
+                default:
+                    return "Error desconocido";
+            }
         }
     },
 
-    // Operación UPDATE (PUT)
+    // Operation update user
     updateUser: async (userData: any, session_token: string) => {
         try {
             const headers = {
@@ -69,9 +109,16 @@ const crud_user = {
             const response = await axios.put(`${API_BASE_URL}/user/`, userData, { headers });
             return response.data;
         } catch (error) {
-            throw error;
+            const responseError = error as { response?: { status?: number; data?: { mensaje?: string } } };
+            if (responseError?.response?.status === 404) {
+                return "Error al actualizar el usuario";
+            } else {
+                return "Error desconocido";
+            }
         }
     },
+
+    // operation update password
     updatePassword: async (userData: any, session_token: string) => {
         try {
             const headers = {
@@ -80,26 +127,32 @@ const crud_user = {
             const response = await axios.put(`${API_BASE_URL}/user/change-password/`, userData, { headers });
             return response.data;
         } catch (error) {
-            throw error;
-        }
-    },
-    // Operación DELETE (DELETE)
-    deleteUser: async (email: string) => {
-        try {
-            const response = await axios.delete(`${API_BASE_URL}/user/${email}/`);
-            return response.data;
-        } catch (error) {
-            throw error;
+            const responseError = error as { response?: { status?: number; data?: { mensaje?: string } } };
+            switch (responseError?.response?.status) {
+                case 400:
+                    return "Error al actualizar la contraseña";
+                case 401:
+                    return "Acceso no autorizado";
+                case 404:
+                    return "Usuario no encontrado";
+                default:
+                    return "Error desconocido";
+            }
         }
     },
 
-    // Operación LIST (GET all)
-    listUser: async () => {
+    // Operación get (GET all featured instructors)
+    getfeaturedInstructors: async () => {
         try {
             const response = await axios.get(`${API_BASE_URL}/user/`);
             return response.data;
         } catch (error) {
-            throw error;
+            const responseError = error as { response?: { status?: number; data?: { mensaje?: string } } };
+            if (responseError?.response?.status === 404) {
+                return "Usuarios no encontrados";
+            } else {
+                return "Error desconocido";
+            }
         }
     },
 };
