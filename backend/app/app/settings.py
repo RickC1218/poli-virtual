c2ac4338bdf17ca41d985349f2f7a40d97b4ff3a
AlexanderG1999 <95202925+AlexanderG1999@users.noreply.github.com>
2024-01-10 22:15:50 -0500

    modify structure with static files

--- backend/app/app/settings.py
+++ backend/app/app/settings.py
@@ -12,10 +12,15 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'assets'),  # Ruta a tu carpeta de activos
+]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
